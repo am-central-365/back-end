491a455f94c48745bd26ec8038e7f7a22cc7b163
aolkhov <aolkhov@gmail.com>
2019-12-29 22:41:36 -0800

    configuring IT tests via Gradle, wip

---
+++ src/testIT/kotlin/com/amcentral365/service/MainKtTest.kt
@@ -0,0 +1,14 @@
+package com.amcentral365.service
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+internal class MainKtTest {
+    @Test
+    fun `irrelevant test`() {
+        print("++ running IT test 'irrelevant test'")
+        val auth = Authorization()
+        val amcUser = AuthenticatedUser("amcentral365", "amcentral365@somemail.com", "amcentral-365 Admin")
+        Assertions.assertTrue(auth.isAdmin(amcUser))
+    }
+}
